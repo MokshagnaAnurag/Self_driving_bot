3f47b7fee1eb4ec8420019723e5c25968a804d7c
Kankati Mokshagna Anurag <116142650+MokshagnaAnurag@users.noreply.github.com>
2025-06-29 15:49:31 +0530

    Add files via upload

---
+++ launch/rplidar.launch.py
@@ -0,0 +1,23 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return LaunchDescription([
+
+        Node(
+            package='rplidar_ros',
+            executable='rplidarNode',
+            name='rplidar_node',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 115200,
+                'frame_id': 'rplidar_link',
+                'angle_compensate': True,
+                'scan_mode': 'Standard'
+            }]
+        )
+    ])
+
